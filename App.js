4c289f5ba042ffd8e582011cc1be400ab0d524d2
Callisto89 <callisto89@users.noreply.github.com>
2018-01-11 23:49:01 +0100

    Backgroundimage Update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Alert, Button, Dimensions, Image, StyleSheet, Text, View } from 'react-
 class BackgroundImage extends React.Component {
   render() {
       return (
-          <Image source={require('./backgroundimage.jpg')}
+          <Image source={require('./images/backgroundimage.jpg')}
                 style={styles.backgroundImage}>
 
                 {this.props.children}
@@ -18,7 +18,15 @@ export default class App extends React.Component {
   render() {
     return (
       <BackgroundImage>
+        <View style={styles.container}>
         <Text style={styles.normalText}>TEST</Text>
+        </View>
+
+        <Image
+        source={require('./images/csgologo.png')}
+        style={styles.csgologoImage}>
+        </Image>
+
       </BackgroundImage>
     );
   }
@@ -28,16 +36,23 @@ const resizeMode = 'contain';
 const dimensions = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  backgroundImage: {
+  container: {
     flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'cover',
+    width: '100%', 
   },
-  csgologoImage: {
-    width: Math.round(dimensions.width * 0.75),
-    height: 200,
-    resizeMode: 'contain',
+    backgroundImage: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        width: dimensions.width,
+        height: dimensions.height,
+        resizeMode: 'cover'
+  },
+    csgologoImage: {
+        width: dimensions.width * 0.75,
+        height: 100,
+        resizeMode: 'contain',
+        marginBottom: 20,
   },
   normalText: {
       textAlign: 'center',
