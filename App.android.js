827b9a82a4f2aca73084f8220f9eac95922c8eec
KittenSoup <freddo@gmail.com>
2018-01-21 21:11:02 +0100

    mashing in some android stuff also
    swajping left & right

---
+++ App.android.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View, Text, ViewPagerAndroid} from 'react-native'
+import StartView from './views/StartView'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ViewPagerAndroid
+        style={{flex: 1}}
+        initialPage={1}>
+        <View style={{alignItems: 'center'}} key="1">
+          <Text>First page</Text>
+        </View>
+        <View style={{alignItems: 'center'}} key="2">
+          <StartView />
+        </View>
+        <View style={{alignItems: 'center'}} key="3">
+          <Text>First page</Text>
+        </View>
+      </ViewPagerAndroid>
+    )
+  }
+}
