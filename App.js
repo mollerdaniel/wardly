11194fa22b274990150863725c00ac20ed933255
KittenSoup <freddo@gmail.com>
2018-01-27 16:17:19 +0100

    Added swipe support for ios/Android (#33)

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { View, Text, StyleSheet, BackAndroid } from 'react-native'
 import StartView from './views/StartView'
+import Swiper from 'react-native-swiper'
+import BackgroundImage from './common/BackgroundImage'
 
 export default class App extends React.Component {
+
+
   render() {
     return (
-      <StartView />
+      <Swiper
+        loop={false}
+        showsPagination={false}
+        index={1}>
+        <BackgroundImage>
+          <Text>Left</Text>
+        </BackgroundImage>
+          <StartView />
+        <BackgroundImage>
+          <Text>Right</Text>
+        </BackgroundImage>
+      </Swiper>
     )
   }
 }
