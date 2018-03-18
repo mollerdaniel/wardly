3e5790c23406d74d63219164a82c43cb4ac78e73
Callisto89 <Callisto89@users.noreply.github.com>
2018-03-18 17:29:17 +0100

    Event list for event view
    Date formatting

--- views/EventView.js
+++ views/EventView.js
@@ -13,8 +13,42 @@ export default class EventView extends React.Component {
         }
     }
 
+    autoCapitalize(text) {
+        return text.slice(0,1).toUpperCase() + text.slice(1, text.length)
+    }
+
     timeFormatter(date) {
-        return date + ''
+        if(typeof(date) === 'number') {
+            date = new Date(date)
+        }
+        let timeString = ''
+        let day = date.getDay()
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        let now = new Date()
+        let tomorrow = new Date()
+        tomorrow.setDate(tomorrow.getDate()+ 1)
+        const timeDiff = date - now
+
+        let twentyFourHours = 60 * 60 * 24
+
+        switch (true) {
+            // Om eventet är idag:
+            case now.getDate() === date.getDate():
+            timeString = 'Idag ' + date.toLocaleTimeString('sv-SE')
+            break;
+             // Om eventet är imorgon:
+             case tomorrow.getDate() === date.getDate():
+             timeString = 'Imorgon ' + date.toLocaleTimeString('sv-SE')
+             break;
+            // Om det är mer än 24 h kvar:
+            case timeDiff > twentyFourHours:
+              timeString = date.toLocaleDateString('sv-SE', {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'}) + ' ' + date.toLocaleTimeString('sv-SE') + ''
+              break;
+            default:
+              timeString = date.toLocaleDateString('sv-SE', {weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric'}) + ' ' + date.toLocaleTimeString('sv-SE') + ''
+          }
+        return this.autoCapitalize(timeString)
     }
 
     render() {
