2087ee2bf8d2f0f18655b89dc77293a78ee944a7
ProHugoLeet <35170497+ProHugoLeet@users.noreply.github.com>
2018-03-31 15:29:11 +0200

    Added update time, remove event,add(player) to backend (#45)
    * Updatetime and removeevent
    
    RemoveEvent
    UpdateTime of Event
    
    * Fixed.
    
    * Addfunction and isAdded(event,Nick)

--- backend/App.js
+++ backend/App.js
@@ -33,38 +33,55 @@ function def_response(error, data) {
 
 function getEvents() {
 	return db.get('events')
-	.value()
+		.value()
 }
+
+function isAdded(eventId, nickname) {
+	//let eventId = eventId
+	let numberOfOccurencesInList = db
+		.get('events')
+		.find({ id: eventId })
+		.pick('playerList')
+		.value()
+	if (numberOfOccurencesInList.playerList.indexOf(nickname) > -1) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
 
 function getEvent(eventId) {
 	return db
-	.get('events')
-	.find({ id: eventId })
-	.value()
+		.get('events')
+		.find({ id: eventId })
+		.value()
 }
 
 function getPlayerList(eventId) {
 	let playerList = db
-	.get('playerlist')
-	.find({ eventId: eventId })
-	.value()
+		.get('events')
+		.find({ id: eventId })
+		.pick('playerList')
+		.value()
 	if (!playerList) {
 		return [];
 	}
-	return playerList
+	return playerList.playerList
 }
 
 // Write defaults to DB if empty
-db.defaults({ events: []})
-  .write()
+db.defaults({ events: [] })
+	.write()
 
 // GET /event
-app.get('/event', function(req, res) {
-	const data = {events: getEvents()}
+app.get('/event', function (req, res) {
+	const data = { events: getEvents() }
 	res.send(def_response(null, data))
 });
 
-app.get('/event/:eventId/details', function(req, res) {
+app.get('/event/:eventId/details', function (req, res) {
 	let eventId = req.params.eventId
 	if (!eventId) {
 		res.send(def_response('missing id'))
@@ -75,15 +92,15 @@ app.get('/event/:eventId/details', function(req, res) {
 		res.send(def_response('id was sent, but what is?!'))
 		return
 	}
-	let playerlist = getPlayerList(eventId)
+	let playerList = getPlayerList(eventId)
 	res.send(def_response(null, {
 		events: [event],
-		playerlist: getPlayerList(eventId),
+		playerList: getPlayerList(eventId),
 	}))
 });
 
 // TODO WIP
-app.post('/event/:eventId/signup', function(req, res) {
+app.post('/event/:eventId/signup', function (req, res) {
 	// this should be fetched from a session or credentials somehow
 	//let tempvarname = req.query.name
 
@@ -101,7 +118,7 @@ app.post('/event/:eventId/signup', function(req, res) {
 
 });
 
-app.post('/event', function(req, res) {
+app.post('/event', function (req, res) {
 	if (!req.body.name) {
 		res.send(def_response('missing name'))
 		return;
@@ -112,33 +129,152 @@ app.post('/event', function(req, res) {
 	}
 	if (!req.body.date_start) {
 		res.send(def_response('missing date_start'))
-		return; 
+		return;
 	}
 	if (!req.body.date_end) {
 		res.send(def_response('missing date_end'))
 		return;
 	}
+	let newPlayerList = []
 	let newEventId = shortid.generate()
 	let event = {
 		id: newEventId,
 		name: req.body.name,
 		guildid: req.body.guildid,
 		date_start: req.body.date_start,
-		date_end: req.body.date_end
+		date_end: req.body.date_end,
+		playerList: newPlayerList,
 	}
 	db
-	.get('events')
-	.push(event)
-	.write()
-	.id
+		.get('events')
+		.push(event)
+		.write()
+		.id
 
 	console.log('saving event to id:')
 	console.log(newEventId)
-    var name = req.body.name;
-    let eventFromDB = getEvent(newEventId);
-    if (!eventFromDB) {
-    	res.send(def_response('data was accepted, but an error happened when we tried to store it. Many brokend backend :('))
-    	return
-    }
-    res.send(def_response(null, {events: [eventFromDB]}))
+	var name = req.body.name;
+	let eventFromDB = getEvent(newEventId);
+	if (!eventFromDB) {
+		res.send(def_response('data was accepted, but an error happened when we tried to store it. Many brokend backend :('))
+		return
+	}
+	res.send(def_response(null, { events: [eventFromDB] }))
+});
+
+app.post('/event/:eventId/removeEvent', function (req, res) {
+	let eventId = req.params.eventId
+	if (!eventId) {
+		res.send(def_response('missing id'))
+		return;
+	}
+	let event = getEvent(eventId);
+	if (!event) {
+		res.send(def_response('id was sent, but what is?!'))
+		return
+	}
+
+	db
+		.get('events')
+		.remove({ id: eventId })
+		.write()
+
+	console.log('removed event')
+	console.log(event)
+	var name = req.body.name;
+	let eventFromDB = getEvent(eventId);
+	res.send(def_response(null, 'Event ' + eventId + 'was successfully removed'))
+});
+
+
+app.post('/event/:eventId/updateTime', function (req, res) {
+	let eventId = req.params.eventId;
+	var startDate = req.body.date_start;
+	var endDate = req.body.date_end;
+
+	if (!eventId) {
+		res.send(def_response('missing id'))
+		return;
+	}
+	let event = getEvent(eventId);
+	if (!event) {
+		res.send(def_response('id was sent, but what is?!'))
+		return
+	}
+
+	if (!startDate && !endDate) {
+		res.send(def_response('date_start or end_date is mandatory'))
+		return;
+	}
+	if (startDate) {
+		db
+			.get('events')
+			.find({ id: eventId })
+			.assign({ date_start: startDate })
+			.write()
+			.id
+
+		console.log('updated ' + eventId + 'date_start to:' + endDate)
+	}
+	if (endDate) {
+		db
+			.get('events')
+			.find({ id: eventId })
+			.assign({ date_end: endDate })
+			.write()
+			.id
+
+		console.log('updated ' + eventId + 'date_end to:' + endDate)
+	}
+
+	let eventFromDB = getEvent(eventId);
+	if (!eventFromDB) {
+		res.send(def_response('data was accepted, but an error happened when we tried to store it. Many brokend backend :('))
+		return
+	}
+	res.send(def_response(null, { events: [eventFromDB] }))
+});
+
+
+
+app.post('/event/:eventId/add', function (req, res) {
+	let eventId = req.params.eventId
+	let nickname = req.body.nickName
+	if (!eventId) {
+		res.send(def_response('missing id'))
+		return
+	}
+	if (!nickname) {
+		res.send(def_response('missing nickname'))
+		return
+	}
+	let event = getEvent(eventId);
+	if (!event) {
+		res.send(def_response('id was sent, but what is?!'))
+		return
+	}
+
+	if (isAdded(eventId, nickname)) {
+		res.send(def_response('Player is already in list!'))
+		return
+	}
+	//WE DO NOT TRUST FRONTEND.
+
+	let playerList = getPlayerList(eventId)
+	playerList.push(nickname)
+	db
+		.get('events')
+		.find({ id: eventId })
+		.assign({ playerList })
+		.write()
+		.id
+
+	console.log('Updated playerlist:')
+	console.log(playerList)
+	let eventFromDB = getEvent(eventId);
+	if (!eventFromDB) {
+		res.send(def_response('data was accepted, but an error happened when we tried to store it. Many brokend backend :('))
+		return
+	}
+	res.send(def_response(null, { events: [eventFromDB] }))
 });
